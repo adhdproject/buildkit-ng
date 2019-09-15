e29ff729163ff0f1926afaa7291f4b9316f19100
ADHD <adhd@ubuntu>
2019-09-15 12:25:22 -0500

    Updated artillery-install with flags

--- scripts/1-annoyance/artillery-install.sh
+++ scripts/1-annoyance/artillery-install.sh
@@ -2,22 +2,55 @@
 
 # Time to install Artillery!
 
+install() {
+	if [ -d "/var/artillery" ]; then
+		echo "Artillery is already installed. Exiting.";
+		exit 2;
+	fi
+	
+	git clone https://github.com/BinaryDefense/artillery.git
+	pushd artillery > /dev/null
+	./setup.py -y
+	popd > /dev/null
+	rm -rf artillery
+
+	echo "
+	Artillery installed!"
+	exit 0
+}
+
+uninstall() {
+	if [ ! -d "/var/artillery" ]; then
+		echo "Artillery is not installed. Nothing to do.";
+		exit 2;
+	fi
+
+	#git clone https://github.com/BinaryDefense/artillery.git
+	#pushd artillery > /dev/null
+	/var/artillery/setup.py -y
+	#popd > /dev/null
+	#rm -rf artillery
+
+	echo "
+	Artillery uninstalled!"
+	exit 0
+}
+
 if [ `whoami` != 'root' ]; then
         echo "Artillery can only be installed with root or sudo.";
 	exit 1
 fi
 
-if [ -d "/var/artillery" ]; then
-	echo "Artillery is already installed. Exiting.";
-	exit 2;
-fi
+case "$1" in
+	-i|--install)
+		install;;
+	-u|--uninstall)
+		uninstall;;
+	*)
+		echo "Usage:
+		sudo ./artillery-install.sh [-i|--install]
+		sudo ./artillery-install.sh [-u|--uninstall]
+		";;
+esac
 
-git clone https://github.com/BinaryDefense/artillery.git
-pushd artillery > /dev/null
-./setup.py -y
-popd > /dev/null
-rm -rf artillery
 
-echo "
-Artillery installed!"
-exit 0
