5e8240115028ba7013bc0931059916eb79427323
ADHD <adhd@ubuntu>
2019-09-16 14:09:50 -0500

    Adjusted Bear Trap script

--- scripts/1-annoyance/bear_trap-install.sh
+++ scripts/1-annoyance/bear_trap-install.sh
@@ -2,19 +2,45 @@
 
 # Time to install Bear Trap!
 
+install() {
+	if [ -d "/opt/beartrap/" ]; then
+		echo "Bear Trap is already installed. Exiting.";
+		exit 2;
+	fi
+
+	git clone https://github.com/chrisbdaemon/beartrap.git
+	mv beartrap/ /opt/
+
+	echo "
+	Bear Trap installed!"
+	exit 0
+}
+
+uninstall() {
+	if [ ! -d "/opt/beartrap" ]; then
+		echo "Bear Trap is not installed. Nothing to do.";
+	fi
+
+	rm -rf /opt/beartrap
+
+	echo "
+	Bear Trap uninstalled!"
+	exit 0
+}
+
 if [ `whoami` != 'root' ]; then
-        echo "Bear Trap can only be installed with root or sudo.";
+        echo "Bear Trap can only be installed or uninstalled with root or sudo.";
 	exit 1
 fi
 
-if [ -d "/opt/beartrap/" ]; then
-	echo "Bear Trap is already installed. Exiting.";
-	exit 2;
-fi
-
-git clone https://github.com/chrisbdaemon/beartrap.git
-mv beartrap/ /opt/
-
-echo "
-Bear Trap installed!"
-exit 0
+case "$1" in
+	-i|--install)
+		install;;
+	-u|--uninstall)
+		uninstall;;
+	*)
+		echo "Usage:
+		sudo ./bear_trap-install.sh [-i|--install]
+		sudo ./bear_trap-install.sh [-u|--uninstall]
+		";;
+esac
