93b831e1f6ee732374736b12d8e43928c060a8c1
martianJay <james@blackhillsinfosec.com>
2019-09-22 11:44:59 -0600

    Added Gcat installer

---
+++ scripts/4-attack/gcat-install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#Installation of gcat
+
+install()
+{
+    if [ -d "/opt/gcat" ]; then
+    echo "Gcat is already installed. Exiting.";
+    exit 2
+    fi
+
+    git clone https://github.com/byt3bl33d3r/gcat
+    mv gcat/ /opt/
+    
+
+    echo "
+    Gcat installed!"
+    exit 0
+}
+
+uninstall()
+{
+    if [ ! -d "/opt/gcat" ]; then
+    echo "Gcat is not installed. Exiting.";
+    exit 2
+    fi
+
+    cd /opt
+    rm -rf gcat
+    echo "gcat uninstalled."
+    exit 0
+}
+
+
+
+if [`whoami` != `root`]; then
+    echo "Gcat can only be installed with root or sudo.";
+    exit 1
+fi
+
+case "$1" in
+	-i|--install)
+		install;;
+	-u|--uninstall)
+		uninstall;;
+	*)
+		echo "Usage:
+		sudo ./gcat-install.sh [-i|--install]
+		sudo ./gcat-install.sh [-u|--uninstall]
+		";;
+esac
