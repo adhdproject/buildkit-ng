4c7f242532bcf9a369018238d7b62976f654347c
ADHD <konselbn0125@uwec.edu>
2019-10-01 12:02:20 -0500

    First draft of Sqlite Bug Server installer

---
+++ scripts/2-attribution/sqlitebugserver-install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Time to install Sqlite Bug Server!
+
+install() {
+	if [ -d "/opt/sqlitebugserver" ]; then
+		echo "Sqlite Bug Server is already installed. Exiting."
+		exit 2
+	fi
+	
+	git clone https://bitbucket.org/zaeyx/sqlitebugserver
+	mv sqlitebugserver/ /opt/
+
+	echo "
+	Sqlite Bug Server installed!"
+	exit 0
+}
+
+uninstall() {
+	if [ ! -d "/opt/sqlitebugserver" ]; then
+		echo "Sqlite Bug Server is not installed. Nothing to do."
+		exit 2
+	fi
+
+	rm -rf /opt/sqlitebugserver
+
+	echo "
+	Sqlite Bug Server uninstalled!"
+	exit 0
+}
+
+if [ `whoami` != 'root' ]; then
+        echo "Sqlite Bug Server can only be installed or uninstalled with root or sudo.";
+	exit 1
+fi
+
+case "$1" in
+	-i|--install)
+		install;;
+	-u|--uninstall)
+		uninstall;;
+	*)
+		echo "Usage:
+		sudo ./sqlitebugserver-install.sh [-i|--install]
+		sudo ./sqlitebugserver-install.sh [-u|--uninstall]
+		";;
+esac
