07f4b7b63f4fd9e7d881649cf7daa1e6ed0a1704
ADHD <konselbn0125@uwec.edu>
2019-10-01 11:48:29 -0500

    Updated prereqs, first draft of honeybadger install

---
+++ scripts/2-attribution/honeybadger-install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Time to install HoneyBadger!
+
+install() {
+	if [ -d "/opt/honeybadgerv3" ]; then
+		echo "HoneyBadger is already installed. Exiting."
+		exit 2
+	fi
+	
+	git clone https://github.com/adhdproject/honeybadger
+	mv honeybadger/ /opt/honeybadgerv3
+
+	echo "
+	HoneyBadger installed!"
+	exit 0
+}
+
+uninstall() {
+	if [ ! -d "/opt/honeybadgerv3" ]; then
+		echo "HoneyBadger is not installed. Nothing to do."
+		exit 2
+	fi
+
+	rm -rf /opt/honeybadgerv3
+
+	echo "
+	HoneyBadger uninstalled!"
+	exit 0
+}
+
+if [ `whoami` != 'root' ]; then
+        echo "HoneyBadger can only be installed or uninstalled with root or sudo.";
+	exit 1
+fi
+
+case "$1" in
+	-i|--install)
+		install;;
+	-u|--uninstall)
+		uninstall;;
+	*)
+		echo "Usage:
+		sudo ./honeybadger-install.sh [-i|--install]
+		sudo ./honeybadger-install.sh [-u|--uninstall]
+		";;
+esac
