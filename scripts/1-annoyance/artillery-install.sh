0fb9f942d6472769c50cb3d7a29f38f72218fa8b
ADHD <konselbn0125@uwec.edu>
2019-10-01 20:47:06 -0500

    Second draft of artillery installer with intelligent error checking.

--- scripts/1-annoyance/artillery-install.sh
+++ scripts/1-annoyance/artillery-install.sh
@@ -2,46 +2,74 @@
 
 # Time to install Artillery!
 
+runcmd() {
+	echo $1
+        $1 2>/dev/null;
+        errors=$(($errors+$?));
+}
+
 install() {
+	errors=0
+
 	if [ -d "/var/artillery" ]; then
-		echo "Artillery is already installed. Exiting."
-		exit 2
+		return 255
 	fi
 	
-	git clone https://github.com/BinaryDefense/artillery.git
-	pushd artillery > /dev/null
-	./setup.py -y
-	popd > /dev/null
-	rm -rf artillery
+	runcmd "git clone https://github.com/BinaryDefense/artillery.git"
+	runcmd "pushd artillery"
+	runcmd "./setup.py -y"
+	runcmd "popd"
+	runcmd "rm -rf artillery"
 
-	echo "
-	Artillery installed!"
-	exit 0
+	return $errors
 }
 
 uninstall() {
+	errors=0
+
 	if [ ! -d "/var/artillery" ]; then
-		echo "Artillery is not installed. Nothing to do."
-		exit 2
+		return 255
 	fi
 
-	/var/artillery/setup.py -y
+	runcmd "/var/artillery/setup.py -y"
 
-	echo "
-	Artillery uninstalled!"
-	exit 0
+	return $errors
 }
 
 if [ `whoami` != 'root' ]; then
-        echo "Artillery can only be installed or uninstalled with root or sudo.";
+        echo "Artillery can only be installed or uninstalled with root or sudo."
 	exit 1
 fi
 
 case "$1" in
 	-i|--install)
-		install;;
+		echo "Installing Artillery..."
+		install
+		ret=$?
+		if [[ $ret > 0 && $ret < 255 ]]; then
+			echo
+			echo "Something went wrong while installing Artillery."
+		elif [ $ret = 255 ]; then
+			echo
+			echo "Artillery is already installed. Exiting."
+		else
+			echo
+			echo "Artillery installed!"
+		fi;;
 	-u|--uninstall)
-		uninstall;;
+		echo "Uninstalling Artillery..."
+		uninstall
+		ret=$?
+		if [[ $ret > 0 && $ret < 255 ]]; then
+			echo
+			echo "Something went wrong while uninstall Artillery."
+		elif [ $ret = 255 ]; then
+			echo
+			echo "Artillery is not installed. Nothing to do."
+		elif [ $ret = 0 ]; then
+			echo
+			echo "Artillery uninstalled!"
+		fi;;
 	*)
 		echo "Usage:
 		sudo ./artillery-install.sh [-i|--install]
