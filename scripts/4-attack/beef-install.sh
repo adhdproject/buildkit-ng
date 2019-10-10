737ad4af73f39eef304ed2025a3797a9bc2962b6
ADHD <konselbn0125@uwec.edu>
2019-10-10 13:37:27 -0500

    Tweaks to beef installer

--- scripts/4-attack/beef-install.sh
+++ scripts/4-attack/beef-install.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
 #Installation of beef 
 
-if [`whoami` != `root`]; then
+install() {
+	if [ -d "/opt/beef" ]; then
+		echo "BeEF is already installed. Exiting.";
+		exit 2;
+	fi
+
+	git clone https://github.com/beefproject/beef
+	mv beef/ /opt/
+	pushd /opt/beef
+	./install
+	popd
+
+	echo "
+	BeEF installed!"
+	exit 0
+}
+
+uninstall() {
+	if [ ! -d "/opt/beef" ]; then
+		echo "BeEF is not installed. Nothing to do.";
+		exit 2;
+	fi
+
+	rm -rf /opt/beef
+
+	echo "
+	BeEF uninstalled!"
+	exit 0
+}
+
+if [ `whoami` != 'root' ]; then
     echo "BeEF can only be installed with root or sudo.";
     exit 1
 fi
 
-if [ -d "/opt/beef" ]; then
-    echo "BeEF is already installed. Exiting.";
-fi
-
-git clone https://github.com/beefproject/beef
-mv beef/ /opt/
-cd /opt/beef
-./install
-
-echo "
-BeEF installed!"
-exit 0
+case "$1" in
+	-i|--install)
+		install;;
+	-u|--uninstall)
+		uninstall;;
+	*)
+		echo "Usage:
+		sudo ./beef-install.sh [-i|--install]
+		sudo ./beef-install.sh [-u|--uninstall]
+		";;
+esac
