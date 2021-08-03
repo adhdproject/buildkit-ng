3c2ffda44e9223e2f95a63b8e2251990fdf67405
sendrublez <48640010+sendrublez@users.noreply.github.com>
2021-08-03 12:35:50 -0700

    Add rita-install.sh. Very minimal install

---
+++ scripts/2-attribution/rita-install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Time to install RITA!
+
+install() {
+	if [ -d "/opt/rita" ]; then
+		echo "RITA is already installed. Exiting."
+		exit 2
+	fi
+	
+	git clone https://github.com/activecm/rita/
+	mv rita/ /opt/
+
+	echo "
+	RITA installed to /opt/rita/!"
+	exit 0
+}
+
+uninstall() {
+	if [ ! -d "/opt/rita" ]; then
+		echo "RITA is not installed. Nothing to do."
+		exit 2
+	fi
+
+	rm -rf /opt/rita
+
+	echo "
+	RITA uninstalled!"
+	exit 0
+}
+
+if [ `whoami` != 'root' ]; then
+        echo "RITA can only be installed or uninstalled with root or sudo.";
+	exit 1
+fi
+
+case "$1" in
+	-i|--install)
+		install;;
+	-u|--uninstall)
+		uninstall;;
+	*)
+		echo "Usage:
+		sudo ./rita-install.sh [-i|--install]
+		sudo ./rita-install.sh [-u|--uninstall]
+		";;
+esac
